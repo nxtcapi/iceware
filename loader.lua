190ec26a25eb6132af0fc905d85c12ea991c78a0
capi <89797002+nxtcapi@users.noreply.github.com>
2022-11-06 14:50:27 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,8 @@
 -- Im lazy to do so will do later :p
+
+if game.PlaceId == 1296881293 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",true))()
+   end
+else
+	game.Players:WaitForChild("LocalPlayer"):Kick("IceWare Doesnt Support this game!")
+setclipboard("https://discord.gg/cM3AQMbdBW")
