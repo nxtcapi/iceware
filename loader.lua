5c365c30f735a5c59a3583e7acb2c68953ce3fe5
capi <89797002+nxtcapi@users.noreply.github.com>
2022-11-06 15:07:53 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,23 @@
--- Im lazy to do so will do later :p
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))() -- notification
 
+-- belfast
 if game.PlaceId == 1296881293 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",true))()
-   end
-else
-	game.Players:WaitForChild("LocalPlayer"):Kick("IceWare Doesnt Support this game!")
-setclipboard("https://discord.gg/cM3AQMbdBW")
+		Rayfield:Notify({
+    Title = "IceWare 🧊",
+    Content = "Game found : Warship RP | WW2 🔱🌊",
+    Duration = 6.5,
+    Image = 4483362458,
+    Actions = { -- Notification Buttons
+        Ignore = {
+            Name = "dismiss",
+            Callback = function()
+                Rayfield:Destroy())
+            end
+		},
+	},
+})
+wait(3)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",true))()
+
+end
