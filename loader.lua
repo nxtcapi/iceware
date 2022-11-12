b19b4ac1b9ae76e5408736a5ea93d25fe85f992b
capi <89797002+nxtcapi@users.noreply.github.com>
2022-11-12 22:18:26 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,23 +1,10 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))() -- notification
+local games = {
+    [{1296881293}] = "https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",
 
--- belfast
-if game.PlaceId == 1296881293 then
-		Rayfield:Notify({
-    Title = "IceWare 🧊",
-    Content = "Game found : Warship RP | WW2 🔱🌊",
-    Duration = 6.5,
-    Image = 4483362458,
-    Actions = { -- Notification Buttons
-        Ignore = {
-            Name = "dismiss",
-            Callback = function()
-                Rayfield:Destroy())
-            end
-		},
-	},
-})
-wait(3)
+}
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",true))()
-
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
 end
