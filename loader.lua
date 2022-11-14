43e7ee41109ed6111247f62b1eff076de56191bc
capi <89797002+nxtcapi@users.noreply.github.com>
2022-11-14 17:48:58 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,12 @@
+-- forked from vynixius
+
 local games = {
     [{1296881293}] = "https://raw.githubusercontent.com/nxtcapi/iceware/main/games/belfast",
     [{155615604}] = "https://raw.githubusercontent.com/nxtcapi/iceware/main/games/prisonlife",
 
 }
 
+wait(.1)
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
         loadstring(game:HttpGet(url))(); break
